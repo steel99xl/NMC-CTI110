d1a10acd1a10963f80755c19ddbb34a4dcdab3f6
steel99xl <steel99xl@gmail.com>
2019-04-01 14:01:31 -0400

    Call Pop Quiz

---
+++ Hello_NMC/Hello_NMC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello_NMC
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello NMC");
+        }
+    }
+}
